5eaf7b5560b197a75e94d2619a30cfcb69d30277
Volodya Rachkin <vova@kpnn.ru>
2020-09-24 14:32:10 +0300

    done task 9

--- seminar02/task06/task06.sh
+++ seminar02/task06/task06.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
-#i dont understand why this pattern dont work on my laptop
-cat urls.txt | grep "https:\/\/([qwertyuiopasdfghjklzxcvbnm1234567890-])+(\.([qwertyuiopasdfghjklzxcvbnm1234567890-])+)+" -n -o > tmp
+cat urls.txt | grep -E "https:\/\/([qwertyuiopasdfghjklzxcvbnm1234567890-]){1,}(\.([qwertyuiopasdfghjklzxcvbnm1234567890-]){1,}){1,}" -n -o > tmp
 sed "s/^/<</" tmp > tmp2
-cat tmp
 sed 's/$/>>/' tmp2
 rm tmp
 rm tmp2
--- seminar02/task09/task09.sh
+++ seminar02/task09/task09.sh
@@ -1,1 +1,2 @@
 #! /bin/bash
+grep -E "([qwertyuiopasdfghjklzxcvbnm1234567890-]){1,}(\.([qwertyuiopasdfghjklzxcvbnm1234567890-]){1,}){0,}\@{1}([qwertyuiopasdfghjklzxcvbnm1234567890-]){1,}(\.([qwertyuiopasdfghjklzxcvbnm1234567890-]){1,}){1,}" -i -o emails.txt
